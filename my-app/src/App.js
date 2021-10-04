843e31cdd448cac1ac349b7394e22d02b8fe5f07
Chris Livingston <clivingston@gmail.com>
2021-10-04 13:19:21 -0700

    push

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import Form from './components/Form'
+import Card from './'
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+        <img src='{}' className="App-logo" alt="logo" />
+        <p>
+          Chris Livingston 
+        </p>
+      </header>
+    </div>
+    </>
+  );
+}
+
+export default App;
